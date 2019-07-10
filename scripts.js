daf98b535d62122970b7d9f59e161236b543bc08
FloydCrenshaw <fmcrenshaw75@gmail.com>
2019-07-09 20:50:26 -0500

    removed imaged from body, placed in Css

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Create a greeter fxn, that will use the person's name and display, "Hello,Mr Crenshaw"in <h1>.
+
+/*
+1. Ask the user for their name.
+2. Put that in memory
+3. Construct or concatenate a string with 'hello' and the value stored in memory for their name.
+4. Select the <h1> on the page.
+5. Replace the value of the <h1> tag.
+*/
+
+const name = prompt('What is ur name?');
+console.log(name);
