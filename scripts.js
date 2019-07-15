fc641a1ffe6f6d92fe2775a0e10b0e9812921bcc
FloydCrenshaw <fmcrenshaw75@gmail.com>
2019-07-14 21:08:36 -0500

    Json was updated. Cleaned up HTML and updated About and Contact page and changed CSS styles

--- scripts.js
+++ scripts.js
@@ -7,6 +7,26 @@
 4. Select the <h1> on the page.
 5. Replace the value of the <h1> tag.
 */
+//will need some type of loop
+
 
 const name = prompt('What is ur name?');
-console.log(name);
+
+
+// console.log(Boolean(0))
+
+// console.log(Boolean(name))
+
+// console.log(name === "Steve");
+
+const x = 6
+
+console.log(x===5, x <= 5, x >= 5);
+
+console.log('5'+Boolean)
+
+console.log(x === 7*3 && x=== 3*7);
+
+if(x===6){
+  console.log('yo mamma');
+}
