24cd9b68f2e74a8a87f56b978b312109888cf070
FloydCrenshaw <fmcrenshaw75@gmail.com>
2019-07-15 20:39:35 -0500

    built export components default.

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default `
+<header>
+<div>
+  <h1>Hello World </h1>
+  <h2>My name is Floyd Crenshaw
+
+</header>
+`;
---
+++ components/Main.js
@@ -0,0 +1,34 @@
+export default `
+ <main>
+<section>
+  <h2>This is my top section</h2>
+
+
+  </p>
+</section>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse tenetur ullam libero dolorum in nobis quo similique, culpa saepe quod, error aspernatur repellat consequuntur sequi iure ratione? Aut, consectetur aliquid?</p>
+@@ -18,5 +25,11 @@ <h2>My name is Floyd Crenshaw</h2>
+<p>Numquam dolore distinctio eum omnis. Temporibus dignissimos fugiat possimus blanditiis, esse vel vero? Cum et vitae ut, magnam quis inventore corrupti voluptatum. Sapiente ad reprehenderit accusamus veniam, autem aperiam ullam!</p>
+<p>Laudantium, maxime libero, a earum vel, aperiam corrupti quia eaque debitis sunt dolores. Assumenda id eos iusto adipisci ut, in voluptatibus architecto earum alias molestiae inventore nam itaque off
+  </section>
+
+  <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+      consequatur, nesciunt eaque sapiente necessitatibus dolores labore
+      dignissimos in enim, fuga porro possimus quod facere vero, asperiores
+      quos cum a consectetur? </p>
+
+      <p>
+          Numquam dolore distinctio eum omnis. Temporibus dignissimos fugiat
+          possimus blanditiis, esse vel vero? Cum et vitae ut, magnam quis
+          inventore corrupti voluptatum. Sapiente ad reprehenderit accusamus
+          veniam, autem aperiam ullam!
+        </p>
+        <p>
+          Laudantium, maxime libero, a earum vel, aperiam corrupti quia eaque
+          debitis sunt dolores. Assumenda id eos iusto adipisci ut, in
+          voluptatibus architecto earum alias molestiae inventore nam itaque
+          officia recusandae!
+        </p>
+      </main>
+`;
