7e4b18eace9b76548cf4c5dd26bc909f5caf3dc3
FloydCrenshaw <fmcrenshaw75@gmail.com>
2019-07-15 20:38:07 -0500

    updated greeting of javascript

--- scripts.js
+++ scripts.js
@@ -7,26 +7,29 @@
 4. Select the <h1> on the page.
 5. Replace the value of the <h1> tag.
 */
-//will need some type of loop
+// will need some type of loop
 
 
-const name = prompt('What is ur name?');
+let fname = prompt('What is ur name?');
+let lname = prompt('What is ur name?');
+
+while(!fname || !lname ){
+   fname = prompt('What is ur name?');
+   lname = prompt('What is ur name?');
+
+
+
+
+
+}document.querySelector(greeting).innerHTML = 'Hey young World';
 
 
 // console.log(Boolean(0))
+
 
 // console.log(Boolean(name))
 
 // console.log(name === "Steve");
 
-const x = 6
-
-console.log(x===5, x <= 5, x >= 5);
-
-console.log('5'+Boolean)
-
-console.log(x === 7*3 && x=== 3*7);
-
-if(x===6){
-  console.log('yo mamma');
 }
+
