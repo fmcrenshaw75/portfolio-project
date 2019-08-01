734c130ecc3d0b0842bb2ea790d70857c1dbed52
FloydCrenshaw <fmcrenshaw75@gmail.com>
2019-07-31 20:44:47 -0500

    updated store with 'links' and 'page'

--- index.js
+++ index.js
@@ -3,16 +3,63 @@ import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+
 // describes the current state of the SPA. We say that we pass'pieces of state'
 const store = {
+    'links': {
+        'primary': [ 'Home', 'About', 'Contact' ],
+        'dropdown': [ 'Projects 1', 'Project 2', 'Project 3' ]
+    },
+
     'home': {
-        'title': 'A New Web Developer'
+        'title': 'A New Web Developer',
+        'page': `<section>
+        <h1>Hello World </h1>
+
+          <h2>  My name is Floyd Crenshaw</h2>
+          <p>I am currently in Savvy Coders class</p>
+        <p>This is my first time taking coding classes</p>
+        <p>I love watching movies</p>
+
+          <h2>This is my top section</h2>
+
+
+          </p>
+        </section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse tenetur ullam libero dolorum in nobis quo similique, culpa saepe quod, error aspernatur repellat consequuntur sequi iure ratione? Aut, consectetur aliquid?</p>
+        @@ -18,5 +25,11 @@ <h2>My name is Floyd Crenshaw</h2>
+        <p>Numquam dolore distinctio eum omnis. Temporibus dignissimos fugiat possimus blanditiis, esse vel vero? Cum et vitae ut, magnam quis inventore corrupti voluptatum. Sapiente ad reprehenderit accusamus veniam, autem aperiam ullam!</p>
+        <p>Laudantium, maxime libero, a earum vel, aperiam corrupti quia eaque debitis sunt dolores. Assumenda id eos iusto adipisci ut, in voluptatibus architecto earum alias molestiae inventore nam itaque off
+          </section>
+
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+              consequatur, nesciunt eaque sapiente necessitatibus dolores labore
+              dignissimos in enim, fuga porro possimus quod facere vero, asperiores
+              quos cum a consectetur? </p>
+
+              <p>
+                  Numquam dolore distinctio eum omnis. Temporibus dignissimos fugiat
+                  possimus blanditiis, esse vel vero? Cum et vitae ut, magnam quis
+                  inventore corrupti voluptatum. Sapiente ad reprehenderit accusamus
+                  veniam, autem aperiam ullam!
+                </p>
+                <p>
+                  Laudantium, maxime libero, a earum vel, aperiam corrupti quia eaque
+                  debitis sunt dolores. Assumenda id eos iusto adipisci ut, in
+                  voluptatibus architecto earum alias molestiae inventore nam itaque
+                  officia recusandae!
+                </p>`
     } ,
     'about': {
-        'title': 'About Page'
+        'title': 'About Page',
+        'page': 'This is the about page'
+
     } ,
     'contact': {
-        'title': 'Contact'
+        'title': 'Contact',
+        'page': 'This is the Contact page'
+
     }
 };
 // function declaration . Render is  used to re-render the page.
@@ -21,10 +68,10 @@ function render(state){
     console.log('state came in as:' , state);
     // We use funtion invocation that actually runs the function ans then 'returns' the markup so that it is properly rendered in the browser.
     document.querySelector('#root').innerHTML = `
-${Navigation(state)}
-${Header(state)}
- ${Main(state)}
-   ${Footer(state)}
+        ${Navigation(state)}
+        ${Header(state)}
+        ${Main(state)}
+        ${Footer(state)}
    `;
     const navItems = document.querySelectorAll('nav > ul > li:not(.dropdown)');
 
@@ -32,13 +79,13 @@ ${Header(state)}
         navItem.addEventListener('click' , function clickHandler(event){
             event.preventDefault();
             // console.log(event.target.textContent.toLowerCase());
-            render(states[event.target.textContent.toLowerCase()]);
+            render(store[event.target.textContent.toLowerCase()]);
         });
     });
 }
 // invokation
 // to render a page we pass in piece of state
-render(states.home);
+render(store.home);
 // The Element wil not exist until page is rendered.
 const navItems = document.querySelectorAll('nav > ul > li:not(.dropdown)');
 
@@ -46,9 +93,10 @@ navItems.forEach(function eventListenerAdder(navItem){
     navItem.addEventListener('click' , function clickHandler(event){
         event.preventDefault();
         // console.log(event.target.textContent.toLowerCase());
-        render(states[event.target.textContent.toLowerCase()]);
+        render(store[event.target.textContent.toLowerCase()]);
     });
 });
+
 // let i = 0;
 // Here the value of i will run from 0 until 3.
 // i will serve as a placeholder to represent the index of the item we need to access from navItems.
